18200a7f68418f63381ba578ed083ce5bcb9e8c1
Jesus <michel.js66@gmail.com>
2020-07-25 09:50:35 -0300

    construcao do layout

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Container, Header, BackIcon, ProfileInfo, BottomMenu, HomeIcon, SearchIcon, BellIcon, EmailIcon } from './styles';
+
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon/>
+        </button>
+        
+        <ProfileInfo>
+          <strong>Michel de Jesus</strong>
+          <span>120 Tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      <BottomMenu>
+        <HomeIcon className="active"/>
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </BottomMenu>
+    </Container>
+  );
+}
+
+export default Main;
